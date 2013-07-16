37b31e36707379e23dd5640788aaec82a2992780
tinasoul <tinapshah@gmail.com>
2013-07-16 11:59:57 -0500

    Created index view.

--- app/controllers/tattoos_controller.rb
+++ app/controllers/tattoos_controller.rb
@@ -1,81 +1,70 @@
 class TattoosController < ApplicationController
 
-# GET /tatoos  # GET /tat_trackers.json
   def index
-    @tat_trackers = TatTracker.all
+    @tattoos = Tattoo.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @tat_trackers }
+      format.json { render json: @tattoos }
     end
   end
 
-  # GET /tat_trackers/1
-  # GET /tat_trackers/1.json
   def show
-    @tat_tracker = TatTracker.find(params[:id])
+    @tattoo = Tattoo.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @tat_tracker }
+      format.json { render json: @tattoo }
     end
   end
 
-  # GET /tat_trackers/new
-  # GET /tat_trackers/new.json
   def new
-    @tat_tracker = TatTracker.new
+    @tattoo = Tattoo.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @tat_tracker }
+      format.json { render json: @tattoo }
     end
   end
 
-  # GET /tat_trackers/1/edit
   def edit
-    @tat_tracker = TatTracker.find(params[:id])
+    @tattoo = Tattoo.find(params[:id])
   end
 
-  # POST /tatoos  # POST /tat_trackers.json
   def create
-    @tat_tracker = TatTracker.new(params[:tat_tracker])
+    @tattoo = Tattoo.new(params[:tattoo])
 
     respond_to do |format|
-      if @tat_tracker.save
-        format.html { redirect_to @tat_tracker, notice: 'Tat tracker was successfully created.' }
-        format.json { render json: @tat_tracker, status: :created, location: @tat_tracker }
+      if @tattoo.save
+        format.html { redirect_to @tattoo, notice: 'Your tattoo was saved to tat_tracker!' }
+        format.json { render json: @tattoo, status: :created, location: @tattoo }
       else
         format.html { render action: "new" }
-        format.json { render json: @tat_tracker.errors, status: :unprocessable_entity }
+        format.json { render json: @tattoo.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /tat_trackers/1
-  # PUT /tat_trackers/1.json
   def update
-    @tat_tracker = TatTracker.find(params[:id])
+    @tattoo = Tattoo.find(params[:id])
 
     respond_to do |format|
-      if @tat_tracker.update_attributes(params[:tat_tracker])
-        format.html { redirect_to @tat_tracker, notice: 'Tat tracker was successfully updated.' }
+      if @tattoo.update_attributes(params[:tattoo])
+        format.html { redirect_to @tattoo, notice: 'Tat tracker was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @tat_tracker.errors, status: :unprocessable_entity }
+        format.json { render json: @tattoo.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tat_trackers/1
-  # DELETE /tat_trackers/1.json
   def destroy
-    @tat_tracker = TatTracker.find(params[:id])
-    @tat_tracker.destroy
+    @tattoo = Tattoo.find(params[:id])
+    @tattoo.destroy
 
     respond_to do |format|
-      format.html { redirect_to tat_trackers_url }
+      format.html { redirect_to tattoos_url }
       format.json { head :no_content }
     end
   end
