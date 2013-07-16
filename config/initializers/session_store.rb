4629c4607e73faf050fbca300da636323ee82057
tinasoul <tinapshah@gmail.com>
2013-07-15 20:00:35 -0500

    Intial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TatTrack::Application.config.session_store :cookie_store, key: '_Tat_track_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TatTrack::Application.config.session_store :active_record_store
