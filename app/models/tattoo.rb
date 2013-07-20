6f6e135d6e2124db3b323bd1bd24000f4d5acc59
tinasoul <tinapshah@gmail.com>
2013-07-20 16:54:13 -0500

    Has many /Belongs to logic in models.

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -1,4 +1,6 @@
 class Tattoo < ActiveRecord::Base
   attr_accessible :artist_id, :date_completed, :date_started, :hours_spent, :inspiration, :location_on_body, :owner_id, :pic
   mount_uploader :pic, PicUploader
+
+  belongs_to :user
 end
